dc15d7d75d99e114cb9b6df1e86eaa121f06eeb3
Kalebe Fukuda <kalebefukudadeoliveira@gmail.com>
2024-02-01 11:20:29 -0300

    nyd

--- common_login/src/App.js
+++ common_login/src/App.js
@@ -1,8 +1,17 @@
+import './index.css'
 
 function App() {
   return (
     <div className="App">
+      <body className="bg-slate-200">
+        
       
+        <main className="w-full min-h-screen flex flex-col items-center justify-center">
+          <section>esquerda</section>
+
+          <section>direita</section>
+        </main>
+      </body>
     </div>
   );
 }
